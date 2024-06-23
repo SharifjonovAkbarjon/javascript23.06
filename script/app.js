d307380f6d2e82438005b3e0867b49436683bbb9
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-06-23 05:48:12 +0500

    javascript

---
+++ script/app.js
@@ -0,0 +1,58 @@
+
+// masala 1
+// {
+//     function mrMs(array){
+//         if([array.gender] == "male"){
+//            console.log(`${array.name}  mister `);
+//            return ""
+//         }else{
+//             console.log(`${array.name}  lady`);
+//             return ""
+//         }
+//     }
+//     console.log( mrMs({name: "Savannah Bond", gender: "female"}))
+//     console.log( mrMs({name: "Eva Elfie", gender: "female"}))
+//     console.log( mrMs({name: "Jonny Sins", gender: "male"}))
+// }
+
+
+    // masala 2
+    
+// {
+
+//     function card(number, illustrate){
+//         if(illustrate){
+//             return number
+//         }else{
+//             let secretNumber = number.slice(0,4) + " **** **** **" + number.substring(17);
+//             return secretNumber
+//         }
+//     }
+
+//     console.log(card("8600 1234 5651 2589", false));
+//     console.log(card("8600 5664 2332 4589", true));
+//     console.log(card("8600 4545 1231 8989", false));
+// }
+    
+
+    // masala 3
+
+// {
+//     function primetr(number, number2){
+//         let combination = 2;
+//         let p = combination * (number + number2);
+//         console.log(p);
+         
+        
+//     }
+
+//     console.log(primetr(5, 6) );
+//     console.log(primetr(12, 8) );
+// }
+
+
+    // masala 4
+
+{
+    
+}
